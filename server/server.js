ead622dfa42c691e6c2b2b46fb047ad952d77f20
tourismdashboard4-max <tourism.dashboard4@gmail.com>
2026-04-05 15:10:50 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ const io = new Server(server, {
       'http://localhost:5175', 
       'http://localhost:5176',
       'http://localhost:5177',
+      'http://localhost:5178',
       'http://localhost:5180',
       'https://tourist-app-api.onrender.com'
     ],
@@ -181,11 +182,12 @@ app.use(helmet({
 app.use(cors({
   origin: [
     'http://localhost:5173', 
-    'http://localhost:5174', 
-    'http://localhost:5175',
-    'http://localhost:5176',
-    'http://localhost:5177',
-    'http://localhost:5180',
+      'http://localhost:5174', 
+      'http://localhost:5175', 
+      'http://localhost:5176',
+      'http://localhost:5177',
+      'http://localhost:5178',
+      'http://localhost:5180',
     'https://tourist-app-api.onrender.com'
   ],
   credentials: true
